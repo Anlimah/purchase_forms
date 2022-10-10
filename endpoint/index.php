b87b93627dc553f5ad1c2c939bf30f6f428cb91b
Anlimah <y.m.ratty7@gmail.com>
2022-10-10 18:00:16 +0000

    Classed payment gateway 4

--- endpoint/index.php
+++ endpoint/index.php
@@ -155,7 +155,7 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
 
 			if (isset($_SESSION['step1Done']) && isset($_SESSION['step2Done']) && isset($_SESSION['step3Done']) && isset($_SESSION['step4Done']) && isset($_SESSION['step5Done']) && isset($_SESSION['step6Done']) && isset($_SESSION['step7Done'])) {
 				if ($_SESSION['step1Done'] == true && $_SESSION['step2Done'] == true && $_SESSION['step3Done'] == true && $_SESSION['step4Done'] == true && $_SESSION['step5Done'] == true && $_SESSION['step6Done'] == true && $_SESSION['step7Done'] == true) {
-					$data = $expose->callOrchardGateway($_SESSION["step1"], $_SESSION["step2"], $_SESSION["step4"], $_SESSION["step6"], $_SESSION["step7"]);
+					$data = $expose->callOrchardGateway($_SESSION["step6"], $_SESSION["step7"]);
 				}
 			}
 		} else {
