64ed8115dd351411f82d76a9127107db0aa2717d
Anlimah <y.m.ratty7@gmail.com>
2022-10-20 15:50:28 +0000

    updated index page redirect url

--- index.php
+++ index.php
@@ -3,5 +3,5 @@ session_start();
 if (isset($_SESSION["vendor_type"]) && !empty($_SESSION["vendor_type"])) {
     header("Location: buy-" . strtolower($_SESSION["vendor_type"]) . "/");
 } else {
-    header("Location: https://admissions.rmuictonline.com/");
+    header("Location: https://forms.rmuictonline.com/buy-online/");
 }
