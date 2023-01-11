ee0d02a647de439d3f5ee536791d196e5f4ed626
Anlimah <y.m.ratty7@gmail.com>
2023-01-11 16:47:25 +0000

    Redesigning UI/UX footer placement 4

--- buy-online/confirm.php
+++ buy-online/confirm.php
@@ -19,47 +19,52 @@ if (isset($_GET['exttrid']) && empty($_GET['exttrid'])) header('Location: index.
 
 <body class="fluid-container">
 
-    <header class="fp-header">
-        <div class="container">
-            <div class="items">
-                <img src="../assets/images/rmu-logo-small.png" style="width: 70px;">
-                <span class="rmu-logo-letter">RMU</span>
+    <div id="wrapper">
+
+        <header class="fp-header">
+            <div class="container">
+                <div class="items">
+                    <img src="../assets/images/rmu-logo-small.png" style="width: 70px;">
+                    <span class="rmu-logo-letter">RMU</span>
+                </div>
             </div>
-        </div>
-    </header>
+        </header>
 
-    <main class="container flex-container" style="margin-bottom: 100px;">
-        <div class="flex-card">
-            <div class="form-card card">
-                <div class="purchase-card-header">
-                    <h1>Payment Status Confirmation</h1>
-                </div>
+        <main class="container flex-container" style="margin-bottom: 100px;">
+            <div class="flex-card">
+                <div class="form-card card">
+                    <div class="purchase-card-header">
+                        <h1>Payment Status Confirmation</h1>
+                    </div>
 
-                <div class="purchase-card-step-info">
-                    <span class="step-capsule">Step Final</span>
-                </div>
+                    <div class="purchase-card-step-info">
+                        <span class="step-capsule">Step Final</span>
+                    </div>
 
-                <hr style="color:#999">
+                    <hr style="color:#999">
 
-                <div class="purchase-card-body">
-                    <div class="pay-status" style="margin: 0px 10%;" style="align-items: baseline;">
-                        <div class="d-flex justify-content-center">
-                            <div class="spinner-border" role="status">
-                                <span class="visually-hidden">Loading...</span>
+                    <div class="purchase-card-body">
+                        <div class="pay-status" style="margin: 0px 10%;" style="align-items: baseline;">
+                            <div class="d-flex justify-content-center">
+                                <div class="spinner-border" role="status">
+                                    <span class="visually-hidden">Loading...</span>
+                                </div>
+                                <p style="margin-left: 10px; margin-top:3px" id="status-out"> Connecting...</p>
                             </div>
-                            <p style="margin-left: 10px; margin-top:3px" id="status-out"> Connecting...</p>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </main>
+        </main>
 
-    <footer class="fp-footer container" style="text-align: center;">
-        <p>For more information and support, reach us: </p>
-        <span><i class="bi bi-telephone-fill" style="color:#003262"></i> (+233) 302 712775; 718225; 714070</span>
-        <span><i class="bi bi-envelope-fill" style="color:#003262"></i> admissions@rmu.edu.gh</span>
-    </footer>
+        <footer class="fp-footer fluid-container text-bg-secondary" style="text-align: center;line-height: 1.2;">
+            <span>For more information and support</span>
+            <div style="font-size: 12px;">
+                <span><i class="bi bi-telephone-fill" style="color:#003262"></i> (+233) 302 712775; 718225; 714070</span> |
+                <span><i class="bi bi-envelope-fill" style="color:#003262"></i> admissions@rmu.edu.gh</span>
+            </div>
+        </footer>
+    </div>
 
     <script src="../js/jquery-3.6.0.min.js"></script>
     <script>
@@ -101,12 +106,8 @@ if (isset($_GET['exttrid']) && empty($_GET['exttrid'])) header('Location: index.
                                 },
                                 success: function(result) {
                                     console.log(result);
-                                    if (result.success) {
-                                        $(".pay-status").html("").append(result.message);
-                                        //$(".pay-status").html("").append(result.message + '<br><div><a href="/">Try again</a></div>');
-                                    } else {
-                                        $(".pay-status").html("").append(result.message + '<br><div><a href="/">Try again</a></div>');
-                                    }
+                                    if (result.success) $(".pay-status").html("").append(result.message);
+                                    else $(".pay-status").html("").append(result.message + '<br><div><a href="/">Try again</a></div>');
                                 },
                                 error: function(error) {
                                     console.log(error);
