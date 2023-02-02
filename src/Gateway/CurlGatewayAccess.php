fc7727ec2caf92095d979835bc057b4d94a11aab
Anlimah <y.m.ratty7@gmail.com>
2023-02-02 17:19:52 +0000

    changed sms gateway to hubtel and redesigned curl function 11

--- src/Gateway/CurlGatewayAccess.php
+++ src/Gateway/CurlGatewayAccess.php
@@ -22,10 +22,6 @@ class CurlGatewayAccess
         $this->curl_array = array(
             CURLOPT_URL => $this->url,
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "POST",
             CURLOPT_POSTFIELDS => $this->payload,
