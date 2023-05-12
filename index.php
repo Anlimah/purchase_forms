0da0832d1424c97c8700a83234c6bb2f09179269
Anlimah <y.m.ratty7@gmail.com>
2023-05-12 00:02:01 +0000

    Resolved changes due to changes in DB and fixed form purchase: edited index redirection

--- index.php
+++ index.php
@@ -3,5 +3,5 @@ session_start();
 if (isset($_SESSION["vendor_type"]) && !empty($_SESSION["vendor_type"])) {
     header("Location: buy-" . strtolower($_SESSION["vendor_type"]) . "/");
 } else {
-    header("Location: https://forms.rmuictonline.com/buy-online/");
+    header("Location: buy-online/");
 }
