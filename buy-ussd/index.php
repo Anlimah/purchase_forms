4ba451b082787ed480b8eaa002e95afadedf7bc0
Anlimah <y.m.ratty7@gmail.com>
2023-05-16 19:44:58 +0000

    USSD code 33

--- buy-ussd/index.php
+++ buy-ussd/index.php
@@ -39,9 +39,9 @@ if (isset($text)) {
         }
         $response .= "99." . " More";
     } elseif ($level[0] != "" && $level[0] != "99" && !$level[1]) {
-        $formInfo = $expose->getFormPriceB($formChosen[$level[0]]);
-        $response = "CON " . $formInfo["name"] . " forms cost GHc " . $formInfo["amount"] . ". Select an option.\n";
-        $response .= "1. Buy\n";
+        //$formInfo = $expose->getFormPriceB();
+        //$response = "CON " . $formInfo["name"] . " forms cost GHc " . $formInfo["amount"] . ". Select an option.\n";
+        $response = "1. Buy {$formChosen[$level[0]]}\n";
         $response .= "2. Cancel";
     } elseif ($level[0] >= 1 && $level[0] <= count($underAndPostFprms) && !$level[1]) {
         $formInfo = $expose->getFormPriceB($level[0]);
