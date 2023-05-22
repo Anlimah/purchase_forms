3e8e5b880768beabb924702353f5c148ccbacd43
Anlimah <y.m.ratty7@gmail.com>
2023-05-22 12:49:28 +0000

    Started AppsNMobile endpoint USSD dev interface 2

--- buy-ussd/index.php
+++ buy-ussd/index.php
@@ -11,10 +11,12 @@ if ($_SERVER["REQUEST_METHOD"] != "POST") die("Invalid request!");
 $sessionId      = $_POST["session_id"];     // Session ID
 $serviceCode    = $_POST["service_code"];   // Service code
 $phoneNumber    = $_POST["msisdn"];         // Phone number
-$text           = $_POST["ussd_body"];      // response text
+$ussdBody       = $_POST["ussd_body"];      // response ussdBody
 $networkCode    = $_POST["nw_code"];        // network code 01 > MTN, 02 > VODA, 
 $msgType        = $_POST["msg_type"];       // Message Type 0, 1, 2
 
-$ussd = new USSDHandler($sessionId, $serviceCode, $phoneNumber, $text, $networkCode, $msgType);
+$ussd = new USSDHandler($sessionId, $serviceCode, $phoneNumber, $ussdBody, $networkCode, $msgType);
 $response = $ussd->control();
+
+header("Content-Type: application/json");
 die($response);
--- src/Controller/USSDHandler.php
+++ src/Controller/USSDHandler.php
@@ -6,16 +6,18 @@ session_start();
 
 use Src\Controller\ExposeDataController;
 use Src\Controller\PaymentController;
-
-$expose = new ExposeDataController();
+use Src\System\DatabaseMethods;
 
 class USSDHandler
 {
+    private $expose = null;
+    private $dm = null;
+
     private $sessionId      = null;
     private $serviceCode    = null;
     private $phoneNumber    = null;
     private $msgType        = null;
-    private $msgBody        = null;
+    private $ussdBody        = null;
     private $networkCode    = null;
     private $payload        = array();
 
@@ -24,22 +26,26 @@ class USSDHandler
     private $userLastName   = null;
     private $userMoMoNum    = null;
 
-    public function __construct($sessionId, $serviceCode, $phoneNumber, $msgType, $msgBody, $networkCode)
+    public function __construct($sessionId, $serviceCode, $phoneNumber, $ussdBody, $networkCode, $msgType)
     {
         $this->sessionId    = $sessionId;
         $this->serviceCode  = $serviceCode;
         $this->phoneNumber  = $phoneNumber;
         $this->msgType      = $msgType;
-        $this->msgBody      = $msgBody;
+        $this->ussdBody      = $ussdBody;
         $this->networkCode  = $networkCode;
+        // Fetch and display all available forms
+        $this->expose = new ExposeDataController();
+        $this->dm = new DatabaseMethods();
     }
 
     public function control()
     {
-        /*if (!isset($this->sessionId) || !isset($this->serviceCode) || !isset($phoneNumber) || !isset($msgBody) || !isset($networkCode))
-            $this->msgBody = "[01] Invalid request!";
-        if (empty($this->sessionId) || empty($this->serviceCode) || empty($phoneNumber) || empty($msgBody) || empty($networkCode))
-            $this->msgBody = "[02] Invalid request!";*/
+        $this->activityLogger();
+        /*if (!isset($this->sessionId) || !isset($this->serviceCode) || !isset($phoneNumber) || !isset($ussdBody) || !isset($networkCode))
+            $this->ussdBody = "[01] Invalid request!";
+        if (empty($this->sessionId) || empty($this->serviceCode) || empty($phoneNumber) || empty($ussdBody) || empty($networkCode))
+            $this->ussdBody = "[02] Invalid request!";*/
 
         // Service unavailable for networks other than MTN & VODA
         if ($this->networkCode  == "03" && $this->networkCode  == "04") {
@@ -48,6 +54,8 @@ class USSDHandler
 
         //
         else {
+            $this->setSessionLevels();
+
             switch ($this->msgType) {
                 case '0':
                     $user = $this->sessionId . ":" . $this->phoneNumber;
@@ -58,7 +66,7 @@ class USSDHandler
 
                 case '1':
 
-                    if (!empty($this->msgBody)) {
+                    if (!empty($this->ussdBody)) {
                         // Set session levels
                         $this->setSessionLevels();
                     }
@@ -77,7 +85,7 @@ class USSDHandler
             "service_code" => $this->serviceCode,
             "msisdn" => $this->phoneNumber,
             "msg_type" => $this->msgType,
-            "ussd_body" => $this->msgBody,
+            "ussd_body" => $this->ussdBody,
             "nw_code" => $this->networkCode,
         );
 
@@ -86,22 +94,20 @@ class USSDHandler
 
     private function unSupportedNetworksResponse()
     {
-        $this->msgBody = "This service is available for only MTN and VODAFONE users. Please visit https://forms.rmuictonline.com to buy a form on all networks";
+        $this->ussdBody = "This service is available for only MTN and VODAFONE users. Please visit https://forms.rmuictonline.com to buy a form on all networks";
         $this->msgType = '2';
     }
 
     private function mainMenuResponse()
     {
         $response  = "Welcome to RMU Online Forms Purchase platform. Select a form to buy.\n";
-        // Fetch and display all available forms
-        $expose = new ExposeDataController();
-        $allForms = $expose->getAvailableForms();
+        $allForms = $this->expose->getAvailableForms();
 
         foreach ($allForms as $form) {
             $response .= $form['id'] . ". " . ucwords(strtolower($form['name'])) . "\n";
         }
 
-        $this->msgBody = $response;
+        $this->ussdBody = $response;
         $this->msgType = '1';
     }
 
@@ -118,21 +124,30 @@ class USSDHandler
     private function continueResponse()
     {
         $expose = new ExposeDataController();
+
         if (isset($_SESSION["level"][0]) && !$_SESSION["level"][1]) {
             $this->msgType = '1';
             $formInfo = $expose->getFormPriceA($_SESSION["level"][0]);
             $response = $formInfo[0]["name"] . " forms cost GHc " . $formInfo[0]["amount"] . ".  Enter 1 to continue.\n";
             $response .= "1. Buy";
-        } elseif (isset($_SESSION["level"][1]) && !$_SESSION["level"][2]) {
+        }
+        //
+        elseif (isset($_SESSION["level"][1]) && !$_SESSION["level"][2]) {
             $this->msgType = '1';
             $response = "Enter your first name.";
-        } else if (isset($_SESSION["level"][2]) && !$_SESSION["level"][3]) {
+        }
+        //
+        else if (isset($_SESSION["level"][2]) && !$_SESSION["level"][3]) {
             $this->msgType = '1';
             $response = "Enter your last name.";
-        } else if (isset($_SESSION["level"][3]) && !$_SESSION["level"][4]) {
+        }
+        //
+        else if (isset($_SESSION["level"][3]) && !$_SESSION["level"][4]) {
             $this->msgType = '1';
             $response = "Enter the Mobile Money number to buy the form. eg 024XXXXXXX";
-        } else if (isset($_SESSION["level"][4]) && !$_SESSION["level"][5]) {
+        }
+        //
+        else if (isset($_SESSION["level"][4]) && !$_SESSION["level"][5]) {
             $this->msgType = '2';
             $phlen = strlen($_SESSION["level"][4]);
             $networks_codes = array(
@@ -193,7 +208,7 @@ class USSDHandler
             $response = "Sorry, the input is not valid.";
         }
 
-        $this->msgBody = $response;
+        $this->ussdBody = $response;
     }
 
     private function validateIntInputs($input)
@@ -212,5 +227,16 @@ class USSDHandler
         $validated_input = (bool) preg_match('/^[A-Za-z-]/', $user_input);
         if ($validated_input) return $user_input;
         return false;
+    }
+
+    private function activityLogger()
+    {
+        $query = "INSERT INTO `ussd_activity_logs` (`session_id`, `service_code`, `msisdn`, `msg_type`, `ussd_body`, `nw_code`) 
+                    VALUES(:si, :sc, :ms, :mt, :ub, :nc)";
+        $params = array(
+            ":si" => $this->sessionId, ":sc" => $this->serviceCode, ":ms" => $this->phoneNumber,
+            ":mt" => $this->msgType, ":ub" => $this->ussdBody, ":nc" => $this->networkCode
+        );
+        $this->dm->inputData($query, $params);
     }
 }
