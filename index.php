cd76ac7d1298950c1a694a04275de23134e08f89
Anlimah <y.m.ratty7@gmail.com>
2023-06-01 10:39:43 +0000

    Redecorated button 3

--- index.php
+++ index.php
@@ -18,8 +18,8 @@
 
         <main class="container flex-container">
             <div class="flex-column align-items-center">
-                <h1 style="text-align: center;">RMU Online Form Purchase</h1>
-                <p>Buy your form online. Choose to pay with mobile money or card.</p>
+                <h1 style="text-align: center;">RMU Form Online</h1>
+                <p>Buy your forms online. Choose to pay with mobile money on all networks or card.</p>
                 <a href="step1.php" class="btn btn-outline-primary" style="padding: 15px; width: 150px !important; text-decoration: none !important;">Buy Now</a>
             </div>
         </main>
